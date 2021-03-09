2829c2a5857122d2441a09694c896489fcad1b6b
Eugene Bobrovnik <eugene.bobrovnik@gmail.com>
2021-03-09 20:30:24 +0300

    feat: add mu solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openBrt;
+  for(let i = 0; i < str.length; i++) {
+    for(let j = 0; j < bracketsConfig.length; j++) {
+      if(str[i] == bracketsConfig[j][0]) {
+        if(str[i] == bracketsConfig[j][1] && stack[stack.length - 1] === str[i]){
+          stack.pop();
+          continue;
+        }
+          stack.push(str[i]);
+          continue;    
+      } 
+      if(str[i] == bracketsConfig[j][1]) {
+        openBrt = stack.pop();
+        if(openBrt !== bracketsConfig[j][0]) {
+          return false;
+        }   
+      }
+    }   
+  }
+  if(stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
